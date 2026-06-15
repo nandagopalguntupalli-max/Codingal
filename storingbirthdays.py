fde762f78851ab041b8c4474ef9a4aaca8eaf9db
nandagopalguntupalli-max <nandagopalguntupalli@gmail.com>
2026-06-14 20:56:52 -0700

    Add files via upload

---
+++ storingbirthdays.py
@@ -0,0 +1,12 @@
+f1="Liam"
+f2="Emma"
+f3="Faizon"
+f4="Mill"
+d1=input("Enter " + f1 + "'s birthdate:")
+d2=input("Enter " + f2 + "'s birthdate:")
+d3=input("Enter " + f3 + "'s birthdate:")
+d4=input("Enter " + f4 + "'s birthdate:")
+print(f1 + "'s birthdate is  on " + d1) 
+print(f2 + "'s birthdate is  on " + d2)
+print(f3 + "'s birthdate is  on " + d3)
+print(f4 + "'s birthdate is  on " + d4)
